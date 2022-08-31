0c5333da93cea9275934ca3040aa2d38bb36da3b
xv7ro <a.k_windsor@protonmail.com>
2022-08-31 10:43:14 +0200

    Carousel working JS>IMAGES

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let images = ['/assets/img1.jpg', '/assets/img2.jpg', '/assets/img3.jpg', '/assets/img4.jpg', '../assets/img5.jpg'];
+let carouselBox = document.getElementById('image-box');
+
+function renderImages() {
+  pushNewImages();
+}
+
+function pushNewImages() {
+  for (let i = 0; i <= images.length; i++) {
+    carouselBox.innerHTML += `
+    <div class="img-box">
+    <img src="${images[i]}">
+    <h3 id="paint-title">Bild Titel</h3>
+  </div>
+  `
+  }
+}
+
+function openImg() {
+
+}
