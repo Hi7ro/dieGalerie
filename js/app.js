374a836bd8c84e7e232df818f30b802fd97c5739
xv7ro <a.k_windsor@protonmail.com>
2022-09-01 21:36:39 +0200

    carousel working ++

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let images = ['/assets/img1.jpg', '/assets/img2.jpg', '/assets/img3.jpg', '/assets/img4.jpg', '../assets/img5.jpg'];
+let images = ['/assets/img1.jpg', '/assets/img2.jpg', '/assets/img3.jpg', '/assets/img4.jpg', '/assets/img5.jpg'];
+
 let carouselBox = document.getElementById('image-box');
 
 function renderImages() {
@@ -6,16 +7,14 @@ function renderImages() {
 }
 
 function pushNewImages() {
-  for (let i = 0; i <= images.length; i++) {
+  for (let i = 0; i < images.length; i++) {
     carouselBox.innerHTML += `
     <div class="img-box">
-    <img src="${images[i]}">
-    <h3 id="paint-title">Bild Titel</h3>
+      <img src="${images[i]}">
+      <h3 id="paint-title">Bild Titel ${i + 1}</h3>
   </div>
   `
   }
 }
 
-function openImg() {
-
-}
+function openImg() {}
